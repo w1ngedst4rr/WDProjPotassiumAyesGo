a1f33e81840005a029b010b5e0a4a30f638ed181
whosmikhail <b2029mrgo@pshs.edu.ph>
2025-12-16 20:33:24 +0800

    efohqiobqevbguwejkvbuqwehn

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 
-            let questionnum = 0
-            let container = document.getElementById('flashcard-container');
-            let qgrouplist = []
-            let agrouplist = []
-            let qgroup = [];
-            let agroup = [];
-            let pastY;
-
-            function update() {
-                localStorage.clear;
-            }
 
