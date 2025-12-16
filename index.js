95e87d856d442eec4a34524809e92135f383ab4f
⋆˚࿔ 𝐚𝐬𝐭é𝐫𝐢 ! 𝜗𝜚˚⋆✮ <b2029lsaayes@pshs.edu.ph>
2025-12-16 21:54:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let questionnum = 0
+let container = document.getElementById('flashcard-container');
+let qgrouplist = []
+let agrouplist = []
+let qgroup = [];
+let agroup = [];
+let pastY;
 
+function update() {
+    localStorage.clear
+}
+
+window.addEventListener("load", () => {
+    const elements = document.querySelectorAll(".fade-up");
+    elements.forEach((el, i) => {
+        setTimeout(() => {
+            el.classList.add("show");
+        }, i * 150);
+    });
+});
 
